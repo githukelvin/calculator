87391e0c30c5f21c47bfa4c70b0be1a59062c376
githukelvin <kelvingithu019@gmail.com>
2023-05-18 09:57:40 +0300

    revisiting the  project  add switcher

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const output = document.querySelector(".output");
+const al = output.value;
+const inp = document.querySelector("input")
 const btns = document.querySelectorAll(".btn");
+let body = document.querySelector("body");
 
 
 // FUNCTION TO PRINT  NUMBERS ON THE SCREEN
@@ -24,3 +27,21 @@ function del(){
     
     output.value = output.value.slice(0,-1);
 }
+
+// toggle{}
+
+inp.addEventListener("input",()=>{
+    inpValue= parseInt(inp.value);
+    if(inpValue ===1){
+        body.classList.remove("theme2");               
+        body.classList.remove("theme3");               
+    }
+    else if(inpValue === 2){
+        body.classList.toggle("theme2");  
+        body.classList.remove("theme3");            
+    }
+    else if(inpValue === 3){
+        body.classList.remove("theme2");  
+        body.classList.toggle("theme3")           
+    }
+})
